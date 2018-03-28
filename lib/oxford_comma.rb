256856665d84a3ebd93e9f1c97995c65e982763d
Shun Yao <ysmiracle@hotmail.com>
2018-03-28 06:35:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  case array.length
+    when 1
+      array.join
+    when 2
+      array.join(" and ")
+    else
+      string = ""
+      index = 0
+      while index < array.length-1
+        string << "#{array[index]}, "
+        index += 1
+      end
+      string << "and #{array[-1]}"
+      string
+    end
 end
